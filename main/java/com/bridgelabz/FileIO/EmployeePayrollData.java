6fab13cb579d931f1c5df17e6549cbbdd6aff546
Smita1706 <smitachinchkar1998@gmail.com>
2022-03-11 00:01:26 +0530

    Added : The use case 1 create employee payroll service to read and write from the console

---
+++ main/java/com/bridgelabz/FileIO/EmployeePayrollData.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.FileIO;
+
+public class EmployeePayrollData {
+	int id;
+	String name;
+	double salary;
+	EmployeePayrollData(int id,String name, double salary){
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "EmployeePayrollData [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+}
